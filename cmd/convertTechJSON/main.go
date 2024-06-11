c6e62560adb1a29a86d5ca9173c7fc0d7b61ef09
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2024-06-11 01:37:38 +0100

    Fixed: Generate categories dynamically instead of statically

--- cmd/convertTechJSON/main.go
+++ cmd/convertTechJSON/main.go
@@ -39,8 +39,13 @@ type Technology struct {
 	Implies []string          `json:"implies"`
 }
 
+type Category struct {
+	Name string `json:"name"`
+}
+
 type Technologies struct {
 	Technologies map[string]Technology `json:"technologies"`
+	Categories   map[string]Category   `json:"categories"`
 }
 
 // Define the structure for the CROWler ruleset
@@ -104,110 +109,6 @@ type PageContentSignature struct {
 type URLMicroSignature struct {
 	Signature  string  `yaml:"value"`
 	Confidence float32 `yaml:"confidence"`
-}
-
-// Define category mappings
-var categoryMappings = map[string]string{
-	"1":   "cms",
-	"3":   "web_frameworks",
-	"4":   "documentation",
-	"5":   "widgets",
-	"6":   "ecommerce",
-	"8":   "miscellaneous",
-	"9":   "payment_processors",
-	"10":  "analytics",
-	"11":  "blogging_platforms",
-	"12":  "javascript_frameworks",
-	"13":  "advertising",
-	"14":  "video_players",
-	"15":  "captchas",
-	"16":  "security",
-	"17":  "databases",
-	"18":  "web_frameworks",
-	"19":  "javascript_libraries",
-	"20":  "editors",
-	"21":  "learning_management_systems",
-	"22":  "web_servers",
-	"23":  "search_engines",
-	"24":  "development",
-	"25":  "augmented_reality",
-	"26":  "file_sharing",
-	"27":  "rich_media",
-	"28":  "operating_systems",
-	"29":  "search_engines",
-	"30":  "load_balancers",
-	"31":  "cdns",
-	"32":  "marketing_automation",
-	"33":  "crm",
-	"34":  "tag_managers",
-	"35":  "translation_tools",
-	"36":  "advertising_networks",
-	"41":  "payment_processors",
-	"42":  "tag_managers",
-	"44":  "personalization",
-	"46":  "dmps",
-	"47":  "cloud_platforms",
-	"48":  "testing_tools",
-	"49":  "static_site_generators",
-	"50":  "feedback_and_surveys",
-	"51":  "portfolio_builders",
-	"52":  "live_chat",
-	"53":  "document_management_systems",
-	"54":  "seo",
-	"55":  "accounting",
-	"56":  "ssl_certificates",
-	"57":  "container_platforms",
-	"58":  "voice_platforms",
-	"59":  "animation_libraries",
-	"60":  "calendar",
-	"62":  "cloud_platforms",
-	"63":  "storage",
-	"64":  "dns",
-	"65":  "audio_libraries",
-	"66":  "performance",
-	"67":  "consent_management",
-	"68":  "accessibility",
-	"69":  "identity_management",
-	"70":  "certificates",
-	"71":  "affiliate_programs",
-	"72":  "scheduling",
-	"73":  "page_builders",
-	"74":  "a_b_testing",
-	"75":  "email_marketing",
-	"76":  "personalization",
-	"77":  "dmps",
-	"78":  "performance",
-	"79":  "anti_tracking",
-	"80":  "themes",
-	"83":  "cryptominers",
-	"84":  "data_management_platforms",
-	"85":  "media_servers",
-	"86":  "customer_data_platforms",
-	"87":  "wordpress_plugins",
-	"88":  "web_hosting",
-	"89":  "learning_management_systems",
-	"90":  "reviews",
-	"91":  "buy_now_pay_later",
-	"92":  "page_speed_optimizers",
-	"93":  "business_intelligence",
-	"94":  "referrals",
-	"95":  "digital_asset_management",
-	"96":  "webmail",
-	"97":  "customer_data_platforms",
-	"98":  "container_management",
-	"99":  "performance_monitoring",
-	"100": "form_builders",
-	"101": "documentation",
-	"102": "return_management",
-	"103": "cobrowsing",
-	"104": "website_builders",
-	"105": "augmented_reality",
-	"106": "content_delivery_networks",
-	"107": "order_management",
-	"108": "anti_fraud",
-	"109": "marketing_intelligence",
-	"110": "form_builders",
-	"111": "donation_management",
 }
 
 func createRule(name string, details Technology) DetectionRule {
@@ -365,21 +266,21 @@ func main() {
 	for name, details := range technologies.Technologies {
 		rule := createRule(name, details)
 		for _, cat := range details.Cats {
-			category, exists := categoryMappings[cat]
+			category, exists := technologies.Categories[cat]
 			if !exists {
 				continue
 			}
 
-			if _, ok := rulesets[category]; !ok {
-				rulesets[category] = Ruleset{
-					RulesetName:   fmt.Sprintf("detect_%s_ruleset", category),
+			if _, ok := rulesets[category.Name]; !ok {
+				rulesets[category.Name] = Ruleset{
+					RulesetName:   fmt.Sprintf("detect_%s_ruleset", strings.ReplaceAll(category.Name, " ", "_")),
 					FormatVersion: "1.0.4",
 					Author:        "Your Name",
 					CreatedAt:     time.Now().Format(time.RFC3339),
-					Description:   fmt.Sprintf("Ruleset to detect %s technologies.", strings.ReplaceAll(category, "_", " ")),
+					Description:   fmt.Sprintf("Ruleset to detect %s technologies.", strings.ReplaceAll(category.Name, "_", " ")),
 					RuleGroups: []RuleGroup{
 						{
-							GroupName:      "detect_web_technologies_" + category,
+							GroupName:      "detect_web_technologies_" + category.Name,
 							IsEnabled:      true,
 							DetectionRules: []DetectionRule{},
 						},
@@ -387,14 +288,17 @@ func main() {
 				}
 			}
 
-			ruleset := rulesets[category]
+			ruleset := rulesets[category.Name]
 			ruleset.RuleGroups[0].DetectionRules = append(ruleset.RuleGroups[0].DetectionRules, rule)
-			rulesets[category] = ruleset
+			rulesets[category.Name] = ruleset
 		}
 	}
 
 	// Write to multiple YAML files
 	for category, ruleset := range rulesets {
+		category = strings.ReplaceAll(category, " ", "-")
+		category = strings.ReplaceAll(category, "/", "-")
+		category = strings.ReplaceAll(category, "\\", "-")
 		fmt.Printf("Writing ruleset for %s...\n", category)
 		filename := fmt.Sprintf((*outPath)+"/detect-%s-ruleset.yaml", category)
 		file, err := os.Create(filename)
